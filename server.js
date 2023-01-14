8ea8e09ca51100dc1aea0f2e39621281feb8bc01
Nitesh Pandey <pandeynitesh1302@gmail.com>
2023-01-14 20:06:52 +0530

    did some http method

--- server.js
+++ server.js
@@ -2,11 +2,38 @@ const http = require("http");
 
 const PORT = 8081;
 
+const todoList = ["Complete Node Byte", "Play Cricket"];
+
 http
     .createServer((request, response)=>{
-    response.writeHead(200, {"Content-Type": "text/html"});
-    response.write("<h1>Hello, this is my first server</h1>")
-    response.end();
+        const {method,url} = request
+        if(url === "/todos"){
+            if(method === "GET"){
+                response.writeHead("200")
+                response.write(todoList.toString())
+            }
+            else if(method === "POST"){
+                let body = "";
+                request
+                    .on("error",(err)=>{
+                        console.error(err);
+                    })
+                    .on("data",(chunks)=>{
+                        body+=chunks;
+                        console.log(chunks);
+                    })
+                    .on("end",()=>{
+                        body = JSON.parse(body);
+                        console.log("data: ", body);
+                    });
+            }else{
+                response.writeHead("500");
+            }
+        }
+        else{
+            response.writeHead("404")
+        }
+        response.end();
     })
     .listen(PORT,()=>{
     console.log(`Server is running in the port: ${PORT}`)
